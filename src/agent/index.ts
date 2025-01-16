02a9fe655be4a881b2d63005b9c5b09f85231b78
imdavyking <yungdynamic53@gmail.com>
2025-01-16 20:33:18 +0100

    updated

--- src/agent/index.ts
+++ src/agent/index.ts
@@ -6,12 +6,68 @@ import erc20abi from "@/assets/json/erc20.json";
 import { Contract } from "starknet";
 import { getStarknet } from "get-starknet";
 import OpenAI from "openai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const openAIApiKey = import.meta.env.VITE_OPENAI_API_KEY;
+
+const geminiApiKeys = [
+  import.meta.env.GEMINI_API_KEY_1 || "API_KEY_1",
+  import.meta.env.GEMINI_API_KEY_2 || "API_KEY_2",
+  import.meta.env.GEMINI_API_KEY_3 || "API_KEY_3",
+  import.meta.env.GEMINI_API_KEY_4 || "API_KEY_4",
+  import.meta.env.GEMINI_API_KEY_5 || "API_KEY_5",
+  import.meta.env.GEMINI_API_KEY_6 || "API_KEY_6",
+  import.meta.env.GEMINI_API_KEY_7 || "API_KEY_7",
+  import.meta.env.GEMINI_API_KEY_8 || "API_KEY_8",
+  import.meta.env.GEMINI_API_KEY_9 || "API_KEY_9",
+  import.meta.env.GEMINI_API_KEY_10 || "API_KEY_10",
+  import.meta.env.GEMINI_API_KEY_11 || "API_KEY_11",
+  import.meta.env.GEMINI_API_KEY_12 || "API_KEY_12",
+  import.meta.env.GEMINI_API_KEY_13 || "API_KEY_13",
+  import.meta.env.GEMINI_API_KEY_14 || "API_KEY_14",
+  import.meta.env.GEMINI_API_KEY_15 || "API_KEY_15",
+  import.meta.env.GEMINI_API_KEY_16 || "API_KEY_16",
+  import.meta.env.GEMINI_API_KEY_17 || "API_KEY_17",
+  import.meta.env.GEMINI_API_KEY_18 || "API_KEY_18",
+  import.meta.env.GEMINI_API_KEY_19 || "API_KEY_19",
+  import.meta.env.GEMINI_API_KEY_20 || "API_KEY_20",
+  import.meta.env.GEMINI_API_KEY_21 || "API_KEY_21",
+  import.meta.env.GEMINI_API_KEY_22 || "API_KEY_22",
+  import.meta.env.GEMINI_API_KEY_23 || "API_KEY_23",
+  import.meta.env.GEMINI_API_KEY_24 || "API_KEY_24",
+  import.meta.env.GEMINI_API_KEY_25 || "API_KEY_25",
+  import.meta.env.GEMINI_API_KEY_26 || "API_KEY_26",
+  import.meta.env.GEMINI_API_KEY_27 || "API_KEY_27",
+  import.meta.env.GEMINI_API_KEY_28 || "API_KEY_28",
+  import.meta.env.GEMINI_API_KEY_29 || "API_KEY_29",
+  import.meta.env.GEMINI_API_KEY_30 || "API_KEY_30",
+  import.meta.env.GEMINI_API_KEY_31 || "API_KEY_31",
+  import.meta.env.GEMINI_API_KEY_32 || "API_KEY_32",
+  import.meta.env.GEMINI_API_KEY_33 || "API_KEY_33",
+  import.meta.env.GEMINI_API_KEY_34 || "API_KEY_34",
+  import.meta.env.GEMINI_API_KEY_35 || "API_KEY_35",
+  import.meta.env.GEMINI_API_KEY_36 || "API_KEY_36",
+  import.meta.env.GEMINI_API_KEY_37 || "API_KEY_37",
+  import.meta.env.GEMINI_API_KEY_38 || "API_KEY_38",
+  import.meta.env.GEMINI_API_KEY_39 || "API_KEY_39",
+  import.meta.env.GEMINI_API_KEY_40 || "API_KEY_40",
+  import.meta.env.GEMINI_API_KEY_41 || "API_KEY_41",
+  import.meta.env.GEMINI_API_KEY_42 || "API_KEY_42",
+  import.meta.env.GEMINI_API_KEY_43 || "API_KEY_43",
+  import.meta.env.GEMINI_API_KEY_44 || "API_KEY_44",
+  import.meta.env.GEMINI_API_KEY_45 || "API_KEY_45",
+  import.meta.env.GEMINI_API_KEY_46 || "API_KEY_46",
+  import.meta.env.GEMINI_API_KEY_47 || "API_KEY_47",
+  import.meta.env.GEMINI_API_KEY_48 || "API_KEY_48",
+  import.meta.env.GEMINI_API_KEY_49 || "API_KEY_49",
+  import.meta.env.GEMINI_API_KEY_50 || "API_KEY_50",
+];
+
 export class LifeSourceAgent {
   tools: any;
   toolsDescription: any;
   executionContext: any;
+  currentApiKeyIndex = 0;
 
   constructor() {
     this.tools = {
@@ -35,6 +91,12 @@ export class LifeSourceAgent {
     };
     this.executionContext = {};
   }
+
+  getNextApiKey = () => {
+    this.currentApiKeyIndex =
+      (this.currentApiKeyIndex + 1) % geminiApiKeys.length; // Circular rotation of API keys
+    return geminiApiKeys[this.currentApiKeyIndex];
+  };
 
   async getSNConnection() {
     let starknet = getStarknet();
@@ -108,19 +170,50 @@ export class LifeSourceAgent {
     await account?.waitForTransaction(approveTx.transaction_hash);
     return approveTx;
   }
+  private async openAILLM(prompt: string): Promise<string | null> {
+    try {
+      const openai = new OpenAI({
+        apiKey: openAIApiKey,
+        dangerouslyAllowBrowser: true,
+      });
+
+      const chatCompletion = await openai.chat.completions.create({
+        messages: [{ role: "user", content: prompt }],
+        model: "gpt-4o-mini",
+      });
+
+      return chatCompletion.choices[0].message.content;
+    } catch (error) {
+      return null;
+    }
+  }
+  private async googleLLM(prompt: string): Promise<string | null> {
+    try {
+      const geminiApiKey = this.getNextApiKey();
+      const googleAI = new GoogleGenerativeAI(geminiApiKey);
+      const model = googleAI.getGenerativeModel({
+        model: "gemini-1.5-flash",
+      });
+
+      const result = await model.generateContent(prompt);
+
+      return result.response.text();
+    } catch (error) {
+      return null;
+    }
+  }
 
   private async promptLLM(prompt: string): Promise<string> {
-    const openai = new OpenAI({
-      apiKey: openAIApiKey,
-      dangerouslyAllowBrowser: true,
-    });
+    const googleLLM = await this.googleLLM(prompt);
+    if (googleLLM) {
+      return googleLLM!;
+    }
+    const openaiLLM = await this.openAILLM(prompt);
+    if (openaiLLM) {
+      return openaiLLM!;
+    }
 
-    const chatCompletion = await openai.chat.completions.create({
-      messages: [{ role: "user", content: prompt }],
-      model: "gpt-4o-mini",
-    });
-
-    return chatCompletion.choices[0].message.content!;
+    return prompt;
   }
   /**
    * Decide the next action to be taken by the agent
@@ -130,11 +223,11 @@ export class LifeSourceAgent {
     context: { [key: string]: any }
   ): Promise<{ [key: string]: any }> {
     const prompt = `
-    STRK_ADDR: 0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d
-    ETH_ADDR: 0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7
+    STRK_ADDR (STARKNET): 0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d
+    ETH_ADDR (ETHEREUM): 0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7
     Task: ${task}
-    Available tools: ${this.toolsDescription}
-    Current context: ${context}
+    Available tools: ${JSON.stringify(this.toolsDescription)}
+    Current context: ${JSON.stringify(context)}
     Determine the next action to take based on the current context and available tools.
     Previous results can be referenced using {{result_X}} where X is the step number.
     Respond in JSON format as:
@@ -145,7 +238,7 @@ export class LifeSourceAgent {
     const nextAction = await this.promptLLM(prompt);
     return JSON.parse(nextAction);
   }
-  private executeAction(
+  private async executeAction(
     action: { [key: string]: any },
     context: { [key: string]: any }
   ) {
@@ -158,9 +251,13 @@ export class LifeSourceAgent {
     }
     let args = action["args"];
     if (typeof args == "string") {
+      // replace context variables in args
       for (const key in context) {
         args = args.replace(`{{${key}}}`, context[key].toString());
       }
+    } else if (typeof args == "object") {
+      // check if type is map convert it to array
+      args = Object.values(args);
     }
     return tool(args);
   }
@@ -175,7 +272,7 @@ export class LifeSourceAgent {
       if (action["tool"] == "TASK_COMPLETE") {
         break;
       }
-      const result = this.executeAction(action, context);
+      const result = await this.executeAction(action, context);
       console.log(`Result: ${result}`);
       step += 1;
       context[`result_${step}`] = result;
