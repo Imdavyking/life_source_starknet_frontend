1972581d9ee97f02888c152cfffb6a1c81a31eae
imdavyking <yungdynamic53@gmail.com>
2025-01-17 09:52:53 +0100

    updated

--- src/agent/plugins/get_usd_to_token_price/index.ts
+++ src/agent/plugins/get_usd_to_token_price/index.ts
@@ -1,11 +1,10 @@
 import { Plugin } from "@elizaos/core";
-import { getUsdToTokenPriceAction } from "./getUsdToTokenPrice"; 
-
+import { getUsdToTokenPriceAction } from "./getUsdToTokenPrice";
 
 export const getUsdToTokenPrice: Plugin = {
   name: "getUsdToTokenPrice",
   description:
-    "arguments: tokenAddress (string), amountInUsd (number or string); returns the amount of tokens equivalent to the USD value,return the amount to approve and donate",
+    "arguments: tokenAddress (string), amountInUsd (number or string); returns the amount of tokens equivalent to the USD value.",
   actions: [getUsdToTokenPriceAction],
   evaluators: [],
   providers: [],
