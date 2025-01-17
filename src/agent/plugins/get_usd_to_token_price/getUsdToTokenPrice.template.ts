f294d8872005f234db8fd9b14baaab18330b8098
imdavyking <yungdynamic53@gmail.com>
2025-01-17 09:56:14 +0100

    updated

--- src/agent/plugins/get_usd_to_token_price/getUsdToTokenPrice.template.ts
+++ src/agent/plugins/get_usd_to_token_price/getUsdToTokenPrice.template.ts
@@ -1,7 +1,10 @@
 export const getUsdToTokenPriceTemplate = `
 Respond with a JSON object containing the token address and amount in USD extracted from the most recent message. 
-STRK_ADDR (STARKNET): 0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d
-ETH_ADDR (ETHEREUM): 0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7
+
+these are known addresses, if you get asked about them, use these:
+- ETH/eth: 0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7
+- STRK/strk: 0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d
+
 If any required argument (tokenAddress or amountInUsd) is missing or invalid, respond with an error.
 
 The response must include:
